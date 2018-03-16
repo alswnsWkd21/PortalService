3892af4776c9c2af0731216f4698c040237ef72f
alswnsWkd21 <djflqjfl0209@gmail.com>
2018-03-16 17:35:22 +0900

    userinfo add기능 추가및 리팩토링수행

--- src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -1,5 +1,6 @@
 package kr.ac.jejunu;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -9,14 +10,36 @@ import static org.hamcrest.CoreMatchers.*;
 
 public class UserDaoTest {
 
+    private UserDao userDao;
+    @Before
+    public void setup(){
+        userDao  = new UserDao();
+    }
+
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
+        userDao = new UserDao();
         int id =1;
         User user = userDao.get(1);
         assertThat(user.getId(), is(    1));
         assertThat(user.getName(), is(  "정민준"));
         assertThat(user.getPassword(), is(  "1234"));
+    }
 
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        user.setName("헐크") ;
+        user.setPassword("1111");
+        Integer id =userDao.insert(user);
+//        User insertedUser =userDao.insert(user);
+//        제대로 됬는지 검증하기 위해
+
+        User insertedUser = userDao.get(id);
+        //제대로된 결과 확인하기
+        assertThat(insertedUser.getId(), is(id));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
 }
